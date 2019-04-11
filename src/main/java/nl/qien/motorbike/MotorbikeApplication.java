baf02e90eff41caee1952125000f630c81c75c51
Raymond Loman <raymond@carpago.nl>
2019-04-11 12:32:36 +0200

    Add a WORKING Swagger implementation which is usable for the CarEndpoint which is a Jersey JAX-RS endpoint

--- src/main/java/nl/qien/motorbike/MotorbikeApplication.java
+++ src/main/java/nl/qien/motorbike/MotorbikeApplication.java
@@ -4,10 +4,25 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.service.StringVendorExtension;
+import springfox.documentation.service.VendorExtension;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
 
 
 @SpringBootApplication
+@EnableSwagger2
 @EnableAspectJAutoProxy
 public class MotorbikeApplication {
 
@@ -18,6 +33,36 @@ public class MotorbikeApplication {
 
 		logger.info("Test zou ik niet moeten zijn want level if fatal");
 		SpringApplication.run(MotorbikeApplication.class, args);
+	}
+
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()            .apis(RequestHandlerSelectors.basePackage("nl.qien.motorbike.api"))
+				.paths(PathSelectors.any())
+				.build()
+				.enable(true)
+				.apiInfo(apiInfo());
+	}
+
+	public ApiInfo apiInfo() {
+		StringVendorExtension vendorExtension = new StringVendorExtension("", "");
+		Collection<VendorExtension> vendorExtensions = new ArrayList<>();
+		vendorExtensions.add(vendorExtension);
+
+		Contact contactInfo = new Contact("AtechRef", "www.atechref.com",
+				"atechsoft@gmail.com");
+
+		return new ApiInfo(
+				"SpringBoot-Swagger2-JaxRS",
+				"Example project showing how to integrate spring boot " +
+						"web app using jaxrs instead of springmvc with swagger and springfox.",
+				"1.0",
+				"For Demo only",
+				contactInfo,
+				"Apache 2.0",
+				"www.apache.org",
+				vendorExtensions);
 	}
 
 }
--- src/main/java/nl/qien/motorbike/config/JerseyConfig.java
+++ src/main/java/nl/qien/motorbike/config/JerseyConfig.java
@@ -1,5 +1,9 @@
 package nl.qien.motorbike.config;
 
+import io.swagger.jaxrs.config.BeanConfig;
+import io.swagger.jaxrs.config.SwaggerConfigLocator;
+import io.swagger.jaxrs.config.SwaggerContextService;
+import io.swagger.jaxrs.listing.ApiListingResource;
 import nl.qien.motorbike.api.CarEndpoint;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.springframework.context.annotation.Configuration;
@@ -12,5 +16,15 @@ public class JerseyConfig extends ResourceConfig {
 
     public JerseyConfig() {
         register(CarEndpoint.class);
+
+        BeanConfig swaggerConfig = new BeanConfig();
+        swaggerConfig.setBasePath("/jersey");
+
+        SwaggerConfigLocator.getInstance().putConfig(SwaggerContextService.CONFIG_ID_DEFAULT, swaggerConfig);
+
+        packages(getClass().getPackage().getName(),
+                ApiListingResource.class.getPackage().getName());
     }
+
+
 }
