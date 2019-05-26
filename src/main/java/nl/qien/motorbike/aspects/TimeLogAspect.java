c216dc229dcebdc25c83b3a59e10d395a0e944d4
Raymond Loman <raymond@carpago.nl>
2019-05-26 11:25:05 +0200

    Remove the lombok @Slf4J annotation in favor of real code (for this example)

--- src/main/java/nl/qien/motorbike/aspects/TimeLogAspect.java
+++ src/main/java/nl/qien/motorbike/aspects/TimeLogAspect.java
@@ -1,15 +1,24 @@
 package nl.qien.motorbike.aspects;
 
-import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+
+
+// Credits NLJUG, from url: https://nljug.org/java-magazine/aspect-oriented-programming-met-spring-boot
+
+//@Slf4j => can also be used to have the log statement below running (as the code example shows)
+
 
 @Aspect
 @Component
-@Slf4j
 public class TimeLogAspect {
+
+
+    private Logger logger = LoggerFactory.getLogger(TimeLogAspect.class);
 
     @Around("@annotation(nl.qien.motorbike.aspects.Timed) && execution(public * *(..))")
     public Object time(final ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
@@ -32,7 +41,7 @@ public class TimeLogAspect {
             // when done ...
             long duration = System.currentTimeMillis() - start;
 
-            log.info("{}.{} took {} ms",
+            logger.info("{}.{} took {} ms",
                     proceedingJoinPoint.getSignature().getDeclaringType().getSimpleName(),
                     proceedingJoinPoint.getSignature().getName(),
                     duration);
