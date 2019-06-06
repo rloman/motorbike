ed4e1f018cafbeecdeba00f2b50b6327039879af
Raymond Loman <raymond@carpago.nl>
2019-06-06 13:31:26 +0200

    Add CRUD for Motorbike

--- src/main/java/nl/qien/motorbike/api/MotorbikeController.java
+++ src/main/java/nl/qien/motorbike/api/MotorbikeController.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Optional;
+
 @RestController
 @RequestMapping("api/motorbikes")
 @CrossOrigin// (origins = {"http://localhost:4200", "http://192.168.1.20:3000"}, methods = {RequestMethod.POST, RequestMethod.PUT}, allowedHeaders = {"content-type"})
@@ -27,6 +29,47 @@ public class MotorbikeController {
     public ResponseEntity<Iterable<Motorbike>> list() {
         return ResponseEntity.ok(this.motorbikeService.findAll());
     }
+
+    @GetMapping("{id}")
+    public ResponseEntity<Motorbike> findById(@PathVariable long id) {
+
+        Optional<Motorbike> result = motorbikeService.findById(id);
+        if(result.isPresent()) {
+            Motorbike motorbike = result.get();
+            return ResponseEntity.ok(motorbike);
+        }
+        else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<Motorbike> updateById(@PathVariable long id, @RequestBody Motorbike motorbikeInput) {
+
+        Optional<Motorbike> result = motorbikeService.findById(id);
+        if(result.isPresent()) {
+            Motorbike motorbike = result.get();
+            motorbike.setOwner(motorbikeInput.getOwner());
+
+            this.motorbikeService.save(motorbike);
+
+            return ResponseEntity.ok(motorbike);
+        }
+        else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("{id}")
+    public void deleteById(@PathVariable long id) {
+
+        Optional<Motorbike> result = motorbikeService.findById(id);
+        if(result.isPresent()) {
+            this.motorbikeService.deleteById(id);
+        }
+    }
+
+
 
     @GetMapping("/printTeam")
     public ResponseEntity<Raceteam> printteam() {
